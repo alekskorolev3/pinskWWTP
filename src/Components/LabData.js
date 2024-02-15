3a2ba1616fbf2742a17bf7137181a57844f89cdb
GorbachyovKS <gorbachyovKS@gmail.com>
2024-02-15 16:28:39 +0300

    table td tr component

--- src/Components/LabData.js
+++ src/Components/LabData.js
@@ -253,6 +253,35 @@ const LabData = () => {
             .catch(() => errorGet())
     }, []);
 
+
+    const bbosName = ['ББО 1', 'ББО 2', 'ББО 3', 'ББО 4' , 'ББО 5', 'ББО 6' ,'ББО 7', 'ББО 8']
+    const paramsName = [
+        {key: 'doseFromWeight',  name : 'Доза ила по массе в аэротенке, г/дм³'},
+        {key: 'doseFromVolume',  name : 'Доза ила по объему, см³/дм³'},
+        {key: 'ashPercent',  name : 'Зольность ила, %'},
+        {key: 'suspendSubstSourceWater',  name : 'Содержание взвешенных веществ в исходной воде, мг/дм³'},
+        {key: 'suspendSubstAfterSettling',  name : 'Содержание взвешенных веществ после первичных отстойников, мг/дм³'},
+        {key: 'suspendSubstInPurified',  name : 'Содержание взвешенных веществ в очищенной воде, мг/дм³'},
+        {key: 'concentrationExcessActive',  name : 'Концентрация избыточного активного ила, г/дм³'},
+        {key: 'xpkInputAero',  name : 'Химическое потребление кислорода на входе Аэротенка - ХПК, мг/дм³'},
+        {key: 'bpkInputOS',  name : 'Биохимическое потребление кислорода (5сут) на входе ОС, мг/дм³'},
+        {key: 'bpkInputBbo',  name : 'Биохимическое потребление кислорода (5сут) на входе Биоблока, мг/дм³'},
+        {key: 'bpkOutputBbo',  name : 'Биохимическое потребление кислорода (5 сут) на выходе Биоблока, мг/дм³'},
+        {key: 'nitrogenAmmoniumInputBbo',  name : 'Азот аммонийный на входе Биоблока, мг/дм³'},
+        {key: 'nitrogenAmmoniumOutputBbo',  name : 'Азот аммонийный на выходе Биоблока, мг/дм³'},
+        {key: 'nitrogenNitriteInputBbo',  name : 'Азот нитритов на входе Биоблока, мг/дм³'},
+        {key: 'nitrogenNitriteOutputBbo',  name : 'Азот нитритов на выходе Биоблока, мг/дм³'},
+        {key: 'nitrogenNitrateInputBbo',  name : 'Азот нитратов на входе Биоблока, мг/дм³'},
+        {key: 'nitrogenNitrateOutputBbo',  name : 'Азот нитратов на выходе Биоблока, мг/дм³'},
+        {key: 'totalNitrogenInputBO',  name : 'Общий азот на участок БО, мг/дм³'},
+        {key: 'organicNitrogenOutputBbo',  name : 'Органический азот на выходе Биоблока, мг/дм³'},
+        {key: 'totalPhosphorusOutput',  name : 'Общий фосфор после первичных отстойников, мг/дм³'},
+        {key: 'totalPhosphorusOutputBO',  name : 'Общий фосфор на выходе участка БО, мг/дм³'},
+        {key: 'phSludge',  name : 'рН иловой смеси в Биоблоке'},
+        {key: 'sludgeTemperature',  name : 'Температура иловой смеси в Биоблоке , °С'},
+        {key: 'oxygenModeNitrificationZone',  name : 'Кислородный режим в зоне нитрификации Биоблока, мг/дм³'}
+    ]
+
     return (
         <>
             {contextHolder}
@@ -261,6 +290,44 @@ const LabData = () => {
                     fontFamily: "Euclid Circular A",
                 }
             }}>
+                <Form className={styles.form}>
+                    <div className={styles.container}>
+                        <div className={styles.tableContainer}>
+                            <table className={styles.table} style={{borderBottom: '0'}}>
+                                <tr>
+                                    <th colSpan={9}>Пинск ОС – Лабораторные данные</th>
+                                </tr>
+                                <tr>
+                                    <th>Параметр</th>
+                                    {bbosName.map((bboName, bboID) => 
+                                        <th key={bboID}>{bboName}</th>
+                                    )}
+                                </tr>
+                                {paramsName.map((param, paramID) => 
+                                    <tr key={paramID}>
+                                        <td>{param.name}</td>
+                                        {bbosName.map((bboName, bboID) => 
+                                            <th key={bboID}>
+                                                <InputNumber
+                                                    value={tableValues[`bbo${bboID + 1}`][param.key]}
+                                                    variant='borderless' min={0} max={200}
+                                                    onChange={(e) => setTableValues(prev => ({
+                                                        ...prev,
+                                                        [`bbo${bboID + 1}`]: {
+                                                            ...prev[`bbo${bboID + 1}`],
+                                                            [param.key]: e
+                                                        }
+                                                    }))}
+                                                />
+                                            </th>
+                                        )}
+                                    </tr>
+                                )}
+                            </table>
+                        </div>
+                    </div>
+                </Form>
+            
                 <Form className={styles.form}>
                     <div className={styles.container}>
                         <div className={styles.tableContainer}>
