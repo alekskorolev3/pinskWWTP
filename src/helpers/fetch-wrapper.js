17382d3c512f38f1f019ce2080b64a3d50f1b2e7
alekskorolev3 <alekskorolev3@gmail.com>
2024-03-04 15:34:40 +0300

    Fixed login and register

--- src/helpers/fetch-wrapper.js
+++ src/helpers/fetch-wrapper.js
@@ -1,6 +1,12 @@
+import {useRecoilState} from "recoil";
+import {authAtom} from "../state/auth";
+
 export { useFetchWrapper };
 
 function useFetchWrapper() {
+
+    const [auth, setAuth] = useRecoilState(authAtom);
+
     return {
         get: request('GET'),
         post: request('POST'),
@@ -11,7 +17,8 @@ function useFetchWrapper() {
     function request(method) {
         return (url, body) => {
             const requestOptions = {
-                method, headers: {}
+                method,
+                headers: authHeader(url)
             };
             if (body) {
                 requestOptions.headers['Content-Type'] = 'application/json';
@@ -28,13 +35,28 @@ function useFetchWrapper() {
     function handleResponse(response) {
         return response.text().then(text => {
             const data = text && JSON.parse(text);
-            
+
             if (!response.ok) {
+                if ([401, 403].includes(response.status) && auth?.token) {
+                    localStorage.removeItem('user');
+                    setAuth(null);
+                    // history.push('/login');
+                }
+
                 const error = (data && data.message) || response.statusText;
                 return Promise.reject(error);
             }
-    
             return data;
         });
-    }    
+    }
+
+    function authHeader() {
+        const token = auth?.access;
+        const isLoggedIn = !!token;
+        if (isLoggedIn) {
+            return { Authorization: `Bearer ${token}` };
+        } else {
+            return {};
+        }
+    }
 }
