66d3f4900ae9353514b39488eab6dfd159a74428
alekskorolev3 <alekskorolev3@gmail.com>
2024-03-06 16:12:54 +0300

    Change dev mode

--- src/helpers/const.js
+++ src/helpers/const.js
@@ -1,4 +1,4 @@
-const dev = true
+const dev = false
 
 export const API = dev ? 'http://192.168.2.42:8000/api' : 'http://127.0.0.1:8000/api'
 export const ADDRESS = dev ? 'http://192.168.2.42:8000' : 'http://127.0.0.1:8000'
